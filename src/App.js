7c99f5847f84529cf91f46a8a4ef38fd5c4d26e5
Achyuth Mohan <achyuthmohan1@gmail.com>
2021-12-02 23:11:32 +0530

    Header created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-
+import './app.css'
+import Header from './components/Header';
 function App() {
+  const data='Achyuth Mohan'
   return (
-    <h1>Hello world</h1>
+   <div>
+      <Header data={data}/>
+     <h1 style={{color:'red',backgroundColor:'green'}}>Hello world</h1>
+      <p className={'hello'}>Heyy guyss</p>
+      <p>{data}</p>
+     
+      <br/>
+      </div>
   );
 }
 
 export default App;
+
